77d6f2837748748d0e35a68f49ec616ac694e456
GCY <gcy626@gmail.com>
2018-09-05 12:40:30 +0800

    FIR Algorithm and Coeff Gen

---
+++ CPP FIR Algorithm/FIR.h
@@ -0,0 +1,25 @@
+#ifndef FIR_H
+#define FIR_H
+
+#include <cstdio>
+
+class FIR
+{
+public:
+	FIR(double *coefficients, unsigned number_of_taps);
+	~FIR();
+
+	double filter(double input);
+
+	void reset();
+
+	unsigned getTaps() {return taps;};
+	
+private:
+	double        *coefficients;
+	double        *buffer;
+	unsigned      taps, offset;
+
+};
+
+#endif
